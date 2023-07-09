4f0e9a7e0fdf26df5e1b0219f7cf9eba49fa15d5
Mickael Sergent <mickael.sergent@free.fr>
2023-07-09 13:05:52 +0200

    complete test script

--- tests/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TESTS_PATH=`dirname $0`
-REQV=${TESTS_PATH}/../bin/reqv
-
-#basic test 1 upstream doc and 1 downstream doc
-${REQV} -p ${TESTS_PATH}/inputs/01_basic_1.SSS_1.SRS/project.yaml -a export -F csv -r "SSS<->SRS" -o ${TESTS_PATH}/outputs/01_basic_1.SSS_1.SRS  -v $@
-
---
+++ tests/launch_tests.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NO_COLOR='\033[0m' # No Color
+
+
+TESTS_PATH=`dirname $0`
+REQV=${TESTS_PATH}/../bin/reqv
+DIFF=diff
+
+# $1 result, $2 test name
+check_result()
+{
+    ret=$?
+    if [[ $1 -eq 0 ]]; then
+        echo -e "${GREEN}TEST_RESULT: '$2' passed.${NO_COLOR}"
+    else
+        echo -e "${RED}TEST_RESULT: '$2' failed.${NO_COLOR}"
+    fi    
+}
+
+check_string_result()
+{
+    if [[ $1 == $2 ]]; then
+        echo -e "${GREEN}TEST_RESULT: '$3' passed.${NO_COLOR}"
+    else
+        echo -e "${RED}TEST_RESULT: '$3' failed.${NO_COLOR}"
+    fi
+}
+
+#basic test 1 upstream doc and 1 downstream doc
+TEST_NAME="basic test - output csv"
+${REQV} -p ${TESTS_PATH}/inputs/01_basic_1.SSS_1.SRS/project.yaml -a export -F csv -r "SSS<->SRS" -o ${TESTS_PATH}/outputs/01_basic_1.SSS_1.SRS  -v
+${DIFF} -r ${TESTS_PATH}/outputs/01_basic_1.SSS_1.SRS ${TESTS_PATH}/expected/01_basic_1.SSS_1.SRS 
+check_result $? "${TEST_NAME}"
+
+TEST_NAME="basic test - missing requirement"
+${REQV} -p ${TESTS_PATH}/inputs/02_basic_missing_req_1.SSS_1.SRS/project.yaml -a export -F csv -r "SSS<->SRS" -o ${TESTS_PATH}/outputs/02_basic_missing_req_1.SSS_1.SRS  -v
+${DIFF} -r ${TESTS_PATH}/outputs/02_basic_missing_req_1.SSS_1.SRS ${TESTS_PATH}/expected/02_basic_missing_req_1.SSS_1.SRS 
+check_result $? "${TEST_NAME}"
+
+TEST_NAME="basic test - missing requirement - check status output"
+# sed command => to remove the color
+r=`${REQV} -p ${TESTS_PATH}/inputs/02_basic_missing_req_1.SSS_1.SRS/project.yaml -a status -r "SSS<->SRS" | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g"`
+EXPECTED_OUTPUT=$(cat <<-END
+document: SRS
+  coverage: 60%
+  number of requirement: 5
+  number of uncovered requirement: 2
+    SRS_REQ_001.1
+    SRS_REQ_002.1
+  derived requirement: 20%
+document: SSS
+  coverage: 75%
+  number of requirement: 4
+  number of uncovered requirement: 1
+    SSS_REQ_002.1
+END
+)
+
+#echo "$r"
+check_string_result "$r" "${EXPECTED_OUTPUT}" "${TEST_NAME}"
+
+
