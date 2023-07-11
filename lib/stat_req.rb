d7ae3b31d3cb4687c6e281b666cf381b9eae1a4c
Mickael Sergent <mickael.sergent@free.fr>
2023-07-11 22:47:00 +0200

    improve readiblity (rubocop)

--- lib/stat_req.rb
+++ lib/stat_req.rb
@@ -2,6 +2,15 @@
 
 # class StatDownstreamReq
 class StatDownstreamReq
+  def initialize
+    @nb_req = 0
+    @nb_uncovered_req = 0
+    @doc_name = nil
+    @uncovered_req_list = []
+    @nb_derived_req = 0
+    @derived_req_list = []
+  end
+
   attr_accessor :nb_req, :nb_uncovered_req, :doc_name, :uncovered_req_list,
                 :nb_derived_req, :derived_req_list
 
@@ -16,6 +25,13 @@ end
 
 # class StatUpstreamReq
 class StatUpstreamReq
+  def initialize
+    @nb_req = 0
+    @nb_uncovered_req = 0
+    @doc_name = nil
+    @uncovered_req_list = []
+  end
+
   attr_accessor :nb_req, :nb_uncovered_req, :doc_name, :uncovered_req_list
 
   def coverage_percent
@@ -54,51 +70,47 @@ class StatReq
 
   def build_downstream_stat
     @traca_report.each_downstream_doc do |downstream_doc|
-      nb_total_req = 0
-      nb_req_derived = 0
-      uncovered_req = 0
-      uncovered_req_list = []
-      derived_req_list = []
-      downstream_doc.each_req_line do |req|
-        nb_total_req += 1
-        if req.derived?
-          nb_req_derived += 1
-          derived_req_list.append req.req_id
-        end
-        if req.covers_empty? && req.derived? == false
-          uncovered_req += 1
-          uncovered_req_list.append req.req_id
-        end
-      end
       result = StatDownstreamReq.new
       result.doc_name = downstream_doc.name
-      result.nb_req = nb_total_req
-      result.nb_uncovered_req = uncovered_req
-      result.uncovered_req_list = uncovered_req_list
-      result.derived_req_list = derived_req_list
-      result.nb_derived_req = nb_req_derived
+      downstream_doc.each_req_line do |req|
+        result.nb_req += 1
+        insert_derived(result, req)
+        insert_uncovered(result, req)
+      end
       @downstream_stat << result
     end
   end
 
+  def insert_derived(downstream_stat, req)
+    return unless req.derived?
+
+    downstream_stat.nb_derived_req += 1
+    downstream_stat.derived_req_list.append req.req_id
+  end
+
+  def insert_uncovered(downstream_stat, req)
+    return unless req.covers_empty? && req.derived? == false
+
+    downstream_stat.nb_uncovered_req += 1
+    downstream_stat.uncovered_req_list.append req.req_id
+  end
+
   def build_upstream_stat
     @traca_report.each_upstream_doc do |upstream_doc|
-      nb_total_req = 0
-      uncovered_req = 0
-      uncovered_req_list = []
-      upstream_doc.each_req_line do |req|
-        nb_total_req += 1
-        if req.covered_empty?
-          uncovered_req += 1
-          uncovered_req_list.append req.req_id
-        end
-      end
       result = StatUpstreamReq.new
       result.doc_name = upstream_doc.name
-      result.nb_req = nb_total_req
-      result.nb_uncovered_req = uncovered_req
-      result.uncovered_req_list = uncovered_req_list
+      upstream_doc.each_req_line do |req|
+        result.nb_req += 1
+        insert_uncovered_ignored_derived(result, req)
+      end
       @upstream_stat << result
     end
+  end
+
+  def insert_uncovered_ignored_derived(upstream_stat, req)
+    return unless req.covered_empty?
+
+    upstream_stat.nb_uncovered_req += 1
+    upstream_stat.uncovered_req_list.append req.req_id
   end
 end
