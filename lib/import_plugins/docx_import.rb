bd1aa3e9405ba1c5053b86b22351f267c990bebb
miko53 <miko53@free.fr>
2023-08-12 22:50:46 +0200

    add docx import parameter, for now style name only

--- lib/import_plugins/docx_import.rb
+++ lib/import_plugins/docx_import.rb
@@ -6,7 +6,7 @@ require_relative 'import_plugin'
 
 # clasx DocStyleRules
 class DocxStyleRules
-  def initialize(name)
+  def initialize(name = nil)
     @name = name
     @is_begin = false
   end
@@ -17,17 +17,37 @@ end
 class DocxImportRules
   def initialize
     @rules = []
-    @req_id_style = DocxStyleRules.new 'REQ_ID'
+    @req_id_style = DocxStyleRules.new
     @req_id_style.is_begin = true
-    @req_title_style = DocxStyleRules.new 'REQ_TITLE'
-    @req_text_style = DocxStyleRules.new 'REQ_TEXT'
-    @req_cov_style = DocxStyleRules.new 'REQ_COV'
-    @req_attributes_style = DocxStyleRules.new 'REQ_ATTRIBUTES'
+    @req_title_style = DocxStyleRules.new
+    @req_text_style = DocxStyleRules.new
+    @req_cov_style = DocxStyleRules.new
+    @req_attributes_style = DocxStyleRules.new
+    @req_end_style = DocxStyleRules.new
   end
 
-  def set_rules(rules = {}); end
+  def set_rules(rules = {})
+    @req_id_style = DocxStyleRules.new
+    @req_id_style.name = rules['req_id_style_name']
+    @req_id_style.is_begin = true
 
-  attr_accessor :req_id_style, :req_title_style, :req_text_style, :req_cov_style, :req_attributes_style
+    @req_title_style = DocxStyleRules.new
+    @req_title_style.name = rules['req_title_style_name']
+
+    @req_text_style = DocxStyleRules.new
+    @req_text_style.name = rules['req_text_style_name']
+
+    @req_cov_style = DocxStyleRules.new
+    @req_cov_style.name = rules['req_cov_style_name']
+
+    @req_attributes_style = DocxStyleRules.new
+    @req_attributes_style.name = rules['req_attributes_style_name']
+
+    @req_end_style = DocxStyleRules.new
+    @req_end_style.name = rules['req_end_style_name']
+  end
+
+  attr_accessor :req_id_style, :req_title_style, :req_text_style, :req_cov_style, :req_attributes_style, :req_end_style
 end
 
 # class DocxImport
@@ -47,11 +67,10 @@ class DocxImport < ImportPlugin
     true
   end
 
-  def parse_doc
+  def parse_doc  # rubocop:disable Metrics/MethodLength, Metrics/AbcSize, Metrics/CyclomaticComplexity
     @current_req = {}
     @current_req_attrs = {}
     @docx.each_paragraph do |p|
-      puts "#{p.text} (#{p.style})"
       case p.style
       when @import_rules.req_id_style.name
         insert_req_id(p.text)
@@ -63,6 +82,8 @@ class DocxImport < ImportPlugin
         insert_req_cov(p.text)
       when @import_rules.req_attributes_style.name
         insert_req_attributes(p.text)
+      when !@import_rules.req_end_style.name.nil? && @import_rules.req_end_style.name
+        insert_req_end(p.text)
       end
     end
   end
@@ -99,4 +120,6 @@ class DocxImport < ImportPlugin
     t = text.split(/[:\t]/)
     @current_req_attrs[t[0].strip.downcase] = t[1].strip
   end
+
+  def insert_req_end(text); end
 end
