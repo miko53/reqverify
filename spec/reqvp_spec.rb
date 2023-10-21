e38ab481cfc6c6ff25714ed5d921b56d9cd2d557
miko53 <miko53@free.fr>
2023-10-21 23:42:07 +0200

    add test for reqvp tool

--- spec/reqvp_spec.rb
+++ spec/reqvp_spec.rb
@@ -85,6 +85,157 @@ describe 'create a project' do # rubocop:disable Metrics/BlockLength
     end
   end
 
+  describe 'add plugin rules for SRS' do # rubocop:disable Metrics/BlockLength
+    before do
+      @argv = []
+      @argv.append 'add_plugin_rule'
+      @argv.append "#{@outputs_path}/req_project_folder/req_project.reqprj"
+      @argv.append 'SRS'
+      @argv.append 'req_id_style_name'
+      @argv.append 'REQ_ID'
+
+      @reqvp = ReqvpMain.new
+      @reqvp.initialize_log_level
+      @operation = @reqvp.analyse_args(@argv)
+    end
+
+    it 'insert rules for SRS ' do # rubocop:disable Metrics/BlockLength
+      expect(@operation).not_to eq(nil)
+      expect(@operation.check_args).to eq(true)
+      @operation.exec
+
+      @argv[3] = 'req_title_style_name'
+      @argv[4] = 'REQ_TITLE'
+      @operation = @reqvp.analyse_args(@argv)
+      expect(@operation).not_to eq(nil)
+      expect(@operation.check_args).to eq(true)
+      @operation.exec
+
+      @argv[3] = 'req_text_style_name'
+      @argv[4] = 'REQ_TEXT'
+      @operation = @reqvp.analyse_args(@argv)
+      expect(@operation).not_to eq(nil)
+      expect(@operation.check_args).to eq(true)
+      @operation.exec
+
+      @argv[3] = 'req_cov_style_name'
+      @argv[4] = 'REQ_COV'
+      @operation = @reqvp.analyse_args(@argv)
+      expect(@operation).not_to eq(nil)
+      expect(@operation.check_args).to eq(true)
+      @operation.exec
+
+      @argv[3] = 'req_attributes_style_name'
+      @argv[4] = 'REQ_ATTRIBUTES'
+      @operation = @reqvp.analyse_args(@argv)
+      expect(@operation).not_to eq(nil)
+      expect(@operation.check_args).to eq(true)
+      @operation.exec
+
+      expect(FileUtils.compare_file("#{@outputs_path}/req_project_folder/req_project.reqprj",
+                                    "#{@expected_path}/04_add_plugin_rules_SRS/req_project.reqprj")).to eq(true)
+    end
+  end
+
+  describe 'add plugin rules for SSS' do # rubocop:disable Metrics/BlockLength
+    before do
+      @argv = []
+      @argv.append 'add_plugin_rule'
+      @argv.append "#{@outputs_path}/req_project_folder/req_project.reqprj"
+      @argv.append 'SSS'
+      @argv.append 'req_id_style_name'
+      @argv.append 'REQ_ID'
+
+      @reqvp = ReqvpMain.new
+      @reqvp.initialize_log_level
+      @operation = @reqvp.analyse_args(@argv)
+    end
+
+    it 'insert rules for SSS ' do # rubocop:disable Metrics/BlockLength
+      expect(@operation).not_to eq(nil)
+      expect(@operation.check_args).to eq(true)
+      @operation.exec
+
+      @argv[3] = 'req_title_style_name'
+      @argv[4] = 'REQ_TITLE'
+      @operation = @reqvp.analyse_args(@argv)
+      expect(@operation).not_to eq(nil)
+      expect(@operation.check_args).to eq(true)
+      @operation.exec
+
+      @argv[3] = 'req_text_style_name'
+      @argv[4] = 'REQ_TEXT'
+      @operation = @reqvp.analyse_args(@argv)
+      expect(@operation).not_to eq(nil)
+      expect(@operation.check_args).to eq(true)
+      @operation.exec
+
+      @argv[3] = 'req_cov_style_name'
+      @argv[4] = 'REQ_COV'
+      @operation = @reqvp.analyse_args(@argv)
+      expect(@operation).not_to eq(nil)
+      expect(@operation.check_args).to eq(true)
+      @operation.exec
+
+      @argv[3] = 'req_attributes_style_name'
+      @argv[4] = 'REQ_ATTRIBUTES'
+      @operation = @reqvp.analyse_args(@argv)
+      expect(@operation).not_to eq(nil)
+      expect(@operation.check_args).to eq(true)
+      @operation.exec
+
+      expect(FileUtils.compare_file("#{@outputs_path}/req_project_folder/req_project.reqprj",
+                                    "#{@expected_path}/05_add_plugin_rules_SSS/req_project.reqprj")).to eq(true)
+    end
+  end
+
+  describe 'add derived rule' do
+    before do
+      @argv = []
+      @argv.append 'add_derived_name'
+      @argv.append "#{@outputs_path}/req_project_folder/req_project.reqprj"
+      @argv.append '[D|d]erived'
+
+      @reqvp = ReqvpMain.new
+      @reqvp.initialize_log_level
+      @operation = @reqvp.analyse_args(@argv)
+    end
+
+    it 'insert rules for derived ' do 
+      expect(@operation).not_to eq(nil)
+      expect(@operation.check_args).to eq(true)
+      @operation.exec
+
+      expect(FileUtils.compare_file("#{@outputs_path}/req_project_folder/req_project.reqprj",
+                                    "#{@expected_path}/06_add_derived_rules/req_project.reqprj")).to eq(true)
+    end
+  end
+
+  describe 'add relationship' do
+    before do
+      @argv = []
+      @argv.append 'add_relationships'
+      @argv.append "#{@outputs_path}/req_project_folder/req_project.reqprj"
+      @argv.append 'SSS->SRS'
+      @argv.append 'SRS'
+      @argv.append 'covered-by'
+      @argv.append 'SSS'
+
+      @reqvp = ReqvpMain.new
+      @reqvp.initialize_log_level
+      @operation = @reqvp.analyse_args(@argv)
+    end
+
+    it 'insert relationship rules' do
+      expect(@operation).not_to eq(nil)
+      expect(@operation.check_args).to eq(true)
+      @operation.exec
+
+      expect(FileUtils.compare_file("#{@outputs_path}/req_project_folder/req_project.reqprj",
+                                    "#{@expected_path}/07_add_relationships/req_project.reqprj")).to eq(true)
+    end
+  end
+
   after(:all) do
     FileUtils.rm_rf("#{@outputs_path}/req_project_folder", secure: true)
   end
